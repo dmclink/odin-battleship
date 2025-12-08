3839608976a4efde3a7a9bf8946706c8e5ea0866
dmclink <135415819+dmclink@users.noreply.github.com>
2025-12-08 21:40:09 +0700

    Initial commit

---
+++ setup/index.js
@@ -0,0 +1,8 @@
+export { setupWebpack } from './webpack.js';
+export { installDependencies } from './packagemanager.js';
+export { updatePackageProperty } from './packagejson.js';
+export {
+	removeFromGitTracking,
+	deleteFilesAndDirs,
+	getProjectName,
+} from './files.js';
