bc360546a9862d6c1ccaa02c17938ddf9caf248d
dmclink <135415819+dmclink@users.noreply.github.com>
2025-12-16 22:41:03 +0700

    feat: add display controller to interact with welcome page

---
+++ src/js/displaycontroller.js
@@ -0,0 +1,35 @@
+import { Events } from './events.js';
+import { em } from './eventemitter.js';
+
+class DisplayController {
+	constructor() {
+		this.welcomeScreen = document.querySelector('#welcome-screen');
+		console.log(this.welcomeScreen);
+		this.main = document.querySelector('#main');
+		this.playerModeBtns = Array.from(document.querySelectorAll('.welcome-player-mode'));
+	}
+
+	bindPlayerModeButtons() {
+		// i coincides with enums for game mode
+		// make sure not to change enums order or button order
+		// TODO: consider moving the enums to constants file and iterating its values
+		for (let i = 0; i < this.playerModeBtns.length; i++) {
+			const button = this.playerModeBtns[i];
+			button.addEventListener('click', () => {
+				em.emit(Events.SELECT_GAME_TYPE, i);
+			});
+		}
+	}
+
+	hideWelcomeScreen() {
+		this.welcomeScreen.classList.add('hidden');
+		this.main.classList.remove('hidden');
+	}
+
+	bindEvents() {
+		this.bindPlayerModeButtons();
+		em.on(Events.GAME_START, this.hideWelcomeScreen.bind(this));
+	}
+}
+
+export { DisplayController };
