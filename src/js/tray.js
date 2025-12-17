278ad11eb930ada6621706d64c582a459dba460a
dmclink <135415819+dmclink@users.noreply.github.com>
2025-12-17 10:03:22 +0700

    fix: catch errors when fetching from tray.html

--- src/js/tray.js
+++ src/js/tray.js
@@ -5,17 +5,20 @@ class BoardTray extends HTMLElement {
 	}
 
 	async connectedCallback() {
-		const resp = await fetch('../html/tray.html');
-		const text = await resp.text();
-
 		const tempContainer = document.createElement('div');
-		tempContainer.innerHTML = text;
+		try {
+			const resp = await fetch('../html/tray.html');
+			const text = await resp.text();
+			tempContainer.innerHTML = text;
 
-		console.log(tempContainer);
-		const template = tempContainer.querySelector('#board-tray-template');
-		const templateContent = template.content;
+			const template = tempContainer.querySelector('#board-tray-template');
+			const templateContent = template.content;
 
-		this.shadowRoot.appendChild(templateContent.cloneNode(true));
+			this.shadowRoot.appendChild(templateContent.cloneNode(true));
+		} catch (err) {
+			console.error('failed to get tray element from tray.html:', err);
+			return;
+		}
 	}
 }
 
